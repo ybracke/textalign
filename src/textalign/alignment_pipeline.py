83cfd56ac1f367bd42b9209df8871e316e312727
Yannic Bracke <yannic.bracke@bbaw.de>
2023-07-27 14:53:39 +0200

    Refactor alignment pipeline
    * Use the split() method of DocSplitter to get split versions of
    the document directly.
    * Update test

--- src/textalign/alignment_pipeline.py
+++ src/textalign/alignment_pipeline.py
@@ -6,86 +6,52 @@ from . import sentences
 from .docsplit import DocSplitter
 from . import util
 
-import itertools
-
 
 class AlignmentPipeline:
     def __init__(self, config: Dict = {}):
         self.config: Dict = config
-        self.source_file: str
-        self.target_file: str
-        # self.source_doc: List[util.Token]
-        # self.target_doc: List[util.Token]
+        self.file_a: str
+        self.file_b: str
+        self.doc_a: List[List[util.Token]]
+        self.doc_b: List[List[util.Token]]
+        self.doc_flat_a: List[util.Token]
+        self.doc_flat_b: List[util.Token]
         self.aligner: Aligner
         self.docsplitter: DocSplitter
 
-    def __call__(
-        self, source_file: str, target_file: str
-    ) -> List[sentences.AlignedSentence]:
-        # 0. Load config
-        # TODO 3 versions of the document text -> not good
-
+    def __call__(self, file_a: str, file_b: str) -> List[sentences.AlignedSentence]:
         # 1. Load files to documents
-        self.source_file = source_file
-        self.target_file = target_file
+        self.file_a = file_a
+        self.file_b = file_b
 
-        # TODO: generalize for non-WASTE input
-        self.source_doc = util.parse_waste_output(self.source_file)
-        self.target_doc = util.parse_waste_output(self.target_file)
+        # Sentencized versions of docs: List[List[util.Token]]
+        # TODO: generalize to non-WASTE input
+        self.doc_a = util.parse_waste_output(self.file_a)
+        self.doc_b = util.parse_waste_output(self.file_b)
 
-        # Flat versions of documents (no sentences)
-        # TODO should this be an instance variable or not?
-        self.source_doc_flat = [tok for sent in self.source_doc for tok in sent]
-        self.target_doc_flat = [tok for sent in self.target_doc for tok in sent]
-
-        # List[str] (not List[util.Token]) versions of the flat documents
-        # TODO should this be an instance variable or not?
-        self.source_doc_flat_strlist = [tok.text for tok in self.source_doc_flat]
-        self.target_doc_flat_strlist = [tok.text for tok in self.target_doc_flat]
+        # Flat versions of docs (no sentences): List[util.Token]
+        self.doc_flat_a = [tok for sent in self.doc_a for tok in sent]
+        self.doc_flat_b = [tok for sent in self.doc_b for tok in sent]
 
         # 2. Create an Aligner object for the entire doc
         self.aligner = Aligner()
 
         # 3. Get split positions of documents
         self.docsplitter = DocSplitter(
-            self.source_doc_flat_strlist,
-            self.target_doc_flat_strlist,
+            [tok.text for tok in self.doc_flat_a],  # List[str]
+            [tok.text for tok in self.doc_flat_b],  # List[str]
             **self.config["splitter"],  # kwargs
         )
 
-        # 4. Iterate over split
-        prev_split_idx_orig = 0
-        prev_split_idx_norm = 0
-
-        # Use chain to append text length as final position to iterator
-        # TODO: this will take the entire rest of source and target doc and
-        # tries to compute alignment. Even if one of them is much longer than
-        # the other
-        for (
-            split_idx_orig,
-            split_idx_norm,
-        ) in itertools.chain(
-            self.docsplitter.iterfind_split_positions(),
-            [(len(self.docsplitter.tokens_a), len(self.docsplitter.tokens_b))],
-        ):
-            # Select the splits
-            split_orig = self.source_doc_flat_strlist[
-                prev_split_idx_orig:split_idx_orig
-            ]
-            split_norm = self.target_doc_flat_strlist[
-                prev_split_idx_norm:split_idx_norm
-            ]
-
+        # 4. Iterate over splits
+        for split_a, split_b in self.docsplitter.split():
             # 5. Create Aligner objects for every split
-            aligner_split = Aligner(split_orig, split_norm)
+            aligner_split = Aligner(split_a, split_b)
             aligner_split.translit_tokens()
             aligner_split.nw_align(**self.config["aligner"])
 
             # 6. Append the alignment for the split to the large aligner
             self.aligner.extend(aligner_split)
-
-            prev_split_idx_orig = split_idx_orig
-            prev_split_idx_norm = split_idx_norm
 
         # 7. Clean alignments for whole document
         self.aligner.get_bidirectional_alignments()
@@ -94,17 +60,13 @@ class AlignmentPipeline:
         # Create a representation where the bitext is
         #   (1) aligned by sentence
         #   (2) serializable (contains whitespace info: `util.Token`)
-        start_idxs_hist = util.get_sentence_start_idxs(
-            self.source_doc
-        )  # List[List[util.Token]]
+        start_idxs_a = util.get_sentence_start_idxs(self.doc_a)
         aligned_sents = sentences.get_aligned_sentences(
             self.aligner.aligned_tokidxs,  # List[AlignedPair]
-            start_idxs_hist,  # List[int]
-            self.source_doc_flat,  # List[util.Token]
-            self.target_doc_flat,  # List[util.Token]
+            start_idxs_a,  # List[int]
+            self.doc_flat_a,  # List[util.Token]
+            self.doc_flat_b,  # List[util.Token]
             reset_tok_idxs=True,
         )
 
         return aligned_sents
-
-        # ? Elsewhere - create serialization ans save to file
--- tests/test_alignment_pipeline.py
+++ tests/test_alignment_pipeline.py
@@ -2,9 +2,10 @@ import textalign
 from textalign import AlignmentPipeline
 
 
+# fake test
 def test_alignment_pipeline() -> None:
-    f_hist = "tests/testdata/simplicissimus_hist.h200.txt"
-    f_norm = "tests/testdata/simplicissimus_norm.h200.txt"
+    f_hist = "tests/testdata/simplicissimus_hist.txt"
+    f_norm = "tests/testdata/simplicissimus_norm.txt"
     config = {
         "aligner": {
             "similarity_func": textalign.aligner.levsim_rescored,
@@ -16,7 +17,7 @@ def test_alignment_pipeline() -> None:
             "max_lev_dist": 3,
             "subseq_len": 7,
             "step_size": 10,
-            "max_len_split": 100,
+            "max_len_split": 1000,
             "apply_translit": True,
         },
         "serialization": {"drop_unaligned": True},
@@ -25,10 +26,12 @@ def test_alignment_pipeline() -> None:
     pipeline = AlignmentPipeline(config)
     aligned_sents = pipeline(f_hist, f_norm)
 
-    assert len(pipeline.aligner.tokens_a) == len(pipeline.source_doc_flat)
-    assert len(pipeline.aligner.tokens_b) == len(pipeline.target_doc_flat)
+    assert len(pipeline.aligner.tokens_a) == len(pipeline.doc_flat_a)
+    assert len(pipeline.aligner.tokens_b) == len(pipeline.doc_flat_b)
 
-    for sent in aligned_sents:
-        sent_hist_ser, sent_norm_ser = sent.serialize()  # **config["serialization"])
-        print(sent_hist_ser)
-        print(sent_norm_ser)
+    # with open("tests/testdata/out/pipeline.sent.02.out", "w", encoding="utf-8") as f:
+    #     for sent in aligned_sents:
+    #         sent_hist_ser, sent_norm_ser = sent.serialize(**config["serialization"])
+    #         f.write(f"{sent_hist_ser}\n")
+    #         f.write(f"{sent_norm_ser}\n")
+    #         f.write("\n")
